f63b67c3e76f942e458b13763708323027f7f0ed
abrahan <abrahandev@gmail.com>
2023-08-26 21:28:23 +0200

    Tailwindcss upgrades

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,18 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
       
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-[#00CFC8]'> Hi, my name is</p>
+        <h1 className='text-4xl sm:text-6xl font-bold text-[#ccd6f6]'>Abrahan Frias</h1>
+        <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>I'm a Front-end Developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700]'>I'm a front-end developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive web applications</p>
+        <div>
+            <button className='text-white border-2 px-6 py-3 my-2 flex items-center rounded-lg hover:bg-[#00CFC8] hover:border-[#00CFC8]'>View Work <HiArrowNarrowRight className='ml-3' /> </button>
+        </div>
+      </div>
     </div>
   )
 }
