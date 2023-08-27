974c0e188f8ffd1f2900baf02e97597030a8d32c
abrahan <abrahandev@gmail.com>
2023-08-27 13:49:02 +0200

    About.jsx create

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,12 @@ const Home = () => {
         <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>I'm a Front-end Developer</h2>
         <p className='text-[#8892b0] py-4 max-w-[700]'>I'm a front-end developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive web applications</p>
         <div>
-            <button className='text-white border-2 px-6 py-3 my-2 flex items-center rounded-lg hover:bg-[#00CFC8] hover:border-[#00CFC8]'>View Work <HiArrowNarrowRight className='ml-3' /> </button>
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center rounded-lg hover:bg-[#00CFC8] hover:border-[#00CFC8]'>
+              View Work
+              <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3' />
+              </span>
+            </button>
         </div>
       </div>
     </div>
