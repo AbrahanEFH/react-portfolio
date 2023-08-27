848dcef665ecde711425fa6c9befa0f130f2d8c0
abrahan <abrahandev@gmail.com>
2023-08-27 15:30:03 +0200

    import img from assets

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import CSS from '../assets/css.png'
+import JavaScript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import GitHub from '../assets/github.png'
+import HTML from '../assets/html.png'
+import TailWind from '../assets/tailwind.png'
+ 
 const Skills = () => {
   return (
-    <div>
+    <div name='skills'>
       
     </div>
   )
