bd2eb44c89153e829e4b6c010b709ffb6e13df77
abrahan <abrahandev@gmail.com>
2023-08-28 17:20:06 +0200

    Pages grip

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Tesla from '../assets/testaLp.png'
+import TicTac from '../assets/tictactoe.png'
 
 const Work = () => {
   return (
@@ -8,25 +10,129 @@ const Work = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-[#00CFC8] text-gray-300'>Work</p>
                 <p className='py-4'>Check out some of my recent work</p>
             </div>
-
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                {/* Container */}
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 p-2 rounded-md'>
+                {/* Grid Item */}
+                <div 
+                 style={{backgroundImage: `url(${Tesla})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
-                    <div>
-                        <span>
-
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Clon Tesla Landing Page
                         </span>
-                        <div>
+                        <div className='pt-8 text-center'>
                             <a href="/">
-                                <button>a</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Page</button>
                             </a>
                             <a href="/">
-                                <button>b</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
                 </div>
+
+                <div 
+                 style={{backgroundImage: `url(${Tesla})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Clon Tesla Landing Page
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Page</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div 
+                 style={{backgroundImage: `url(${Tesla})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Clon Tesla Landing Page
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Page</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div 
+                 style={{backgroundImage: `url(${Tesla})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Clon Tesla Landing Page
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Page</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div 
+                 style={{backgroundImage: `url(${Tesla})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Clon Tesla Landing Page
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Page</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div 
+                 style={{backgroundImage: `url(${Tesla})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Clon Tesla Landing Page
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Page</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
