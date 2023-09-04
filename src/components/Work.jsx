d9a3bfebae63f7132d7ab8c5eba8bc418472f326
abrahan <abrahandev@gmail.com>
2023-09-04 16:21:59 +0200

    App change

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,7 +3,7 @@ import Tesla from '../assets/testaLp.png'
 import TicTac from '../assets/tictactoe.png'
 import Agenda from '../assets/agenda.png'
 import Countdown from '../assets/countdown.png'
-import SoundBar from '../assets/soundbar.png'
+import passWord from '../assets/password.png'
 import DarkMode from '../assets/darkmode.png'
 
 
@@ -91,16 +91,16 @@ const Work = () => {
                 </div>
 
                 <div 
-                 style={{backgroundImage: `url(${SoundBar})`}}
+                 style={{backgroundImage: `url(${passWord})`}}
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            Sound Bar
+                            Password Generator
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="https://luminous-gnome-2c124f.netlify.app/" target="_blank">
+                            <a href="https://unique-profiterole-59c788.netlify.app/" target="_blank">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Page</button>
                             </a>
 
